2f85885f4fe7096f5ed842c88078db23c367902f
Kater <kater@DESKTOP-GKQN8IQ.localdomain>
2022-04-21 23:12:23 -0300

    opa esqueci desse treco

---
+++ game.h
@@ -0,0 +1,56 @@
+#ifndef BDASH_H
+#define BDASH_H
+
+#include "lista.h"
+#include "init_sprites.h"
+
+#define KEY_SEEN 1
+#define KEY_RELEASED 2
+
+#define LIN 23
+#define COL 40
+
+#define STILL 0
+#define UP 1
+#define RIGHT 2
+#define DOWN 3
+#define LEFT 4
+
+#define ESQUERDA -1
+#define DIREITA 1
+
+#define OFF 16 // offset para dar espaço ao HUD
+
+typedef struct level{
+    int n_diams, n_rocks, time;
+}level;
+
+typedef struct jogador{
+    int x, y;
+    int score, dir;
+}jogador;
+
+typedef struct tile{
+    enum{Empty = 0, Wall, Dirt, Player, Rock, Exit, Brick, Diamond}tipo;
+}tile;
+
+typedef struct jogo{
+    int n_level, d_restantes, t_restante; 
+}jogo;
+
+typedef struct nodo{
+    int caindo, x, y; //att = se precisa atualizar ou não
+    struct nodo *next;
+    struct nodo *prev;
+}nodo;
+
+tile **aloca_area(int lin, int col);
+void inicializa_jogo(tile **area, jogador *player, nodo **pedras, nodo **diamantes, int nlevel);
+int colisao(tile **area, int direcao, jogador player);
+void move_player(tile **area, jogador *player, int dir);
+int empurra(tile **area, jogador *player, nodo *pedras, int dir);
+void desenha_mapa(tile **area, t_sprites sprites);
+void testa_init(bool test, const char *objeto);
+int morte(jogador player, t_sprites sprites, ALLEGRO_TIMER *t);
+
+#endif
